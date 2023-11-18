9381e62313b9b1516737be25f08e803106c4d70a
Tarek Abdo <tarekk.abdo22@gmail.com>
2023-11-18 14:09:36 +0200

    use children prop

--- src/AppV1.js
+++ src/AppV1.js
@@ -30,42 +30,27 @@ const App = () => {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          {/* <p className="message">
-            <h3>Step {step}:</h3> {messages[step - 1]}
-          </p> */}
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={hanndlePrev}>
-              <span>👈🏻</span> Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next <span>👉🏻</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={hanndlePrev}
+            >
+              Previos
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
   );
 };
-
-function StepMessage({ children, step }) {
-  return (
-    <div className="message">
-      <h3>Step {step}:</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-}
 
 export default App;
